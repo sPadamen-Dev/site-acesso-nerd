a1c566906349cf21acc7a238929608274ec55770
William <willxis@gmail.com>
2021-06-10 00:02:21 -0300

    Defining products route

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const loginRouter = require('./src/routes/login.router')
+const productsRouter = require('./src/routes/products.router')
+
 app.set('view engine', 'ejs')
 app.set('views', './src/views')
-
+app.use(express.json());
 
 // Static Files
 app.use(express.static('public'));
@@ -16,10 +19,8 @@ app.use('/img', express.static(__dirname + 'public/img'));
 app.get('/', (req,res)=>{
     res.render('home')
 })
-
-app.get('/login', (req,res)=>{
-    res.render('login')
-})
+app.use('/login', loginRouter)
+app.use('/products', productsRouter)
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`)
