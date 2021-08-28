3c923c10b909ffa04bfef10bf24035f291b652ce
Rayssa Coutinho <rayccoutinho@gmail.com>
2021-08-28 19:52:21 -0300

    Merge develop

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
-=======
-const port = process.env.PORT || 3000;
->>>>>>> develop
 
 const loginRouter = require('./src/routes/login.router')
 const singUpRouter = require('./src/routes/register.user.router')
