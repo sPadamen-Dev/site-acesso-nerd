c32874b574c1488251277df9719adf527f0f6df8
Rayssa Coutinho <rayccoutinho@gmail.com>
2021-10-15 21:48:18 -0300

    Correções nos formulários

--- public/js/form-validation.js
+++ public/js/form-validation.js
@@ -106,6 +106,56 @@ $(document).ready(function validateForm() {
 
 // ===========================================================// 
 
+function continue2Form(){
+	$(".continue2").on('click',function () {
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	next_fs.show(); 
+	
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			scale = 1 - (1 - now) * 0.2;
+			left = (now * 50)+"%";
+			opacity = 1 - now;
+			current_fs.css({
+				'transform': 'scale('+scale+')',
+				'position': 'absolute'
+			  });
+			  next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		easing: 'easeInOutBack'
+	});
+
+});
+
+}
+
+
+$(document).ready(function validateForm() {
+	$(".continue2").on('click', function () {
+	  var current = $(this).data('currentBlock'),
+		next = $(this).data('nextBlock');
+  		if (next > current)
+		if (false === $('#msform').parsley().validate('block' + current))
+		  return;
+		  continue2Form();
+  	});
+  });
+
+
+// ===========================================================// 
+
 $(".previous").click(function(){
 	if(animating) return false;
 	animating = true;
